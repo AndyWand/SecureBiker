eac1a59ec39bea3f1255ef042ea161a0d0aaa7b4
AndyWand <andreaswandert@gmail.com>
2015-12-09 15:34:02 +0100

    Working Version

--- app/src/main/java/com/example/andreas/securebiker/HelperClass.java
+++ app/src/main/java/com/example/andreas/securebiker/HelperClass.java
@@ -3,6 +3,8 @@ package com.example.andreas.securebiker;
 import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
@@ -16,12 +18,15 @@ import java.util.ArrayList;
 public class HelperClass extends AppCompatActivity {
 
 
-    public ArrayList<Point> getExample() {
-        ArrayList<Point> list = new ArrayList<>();
-        for (int i = 0; i < getResources().getInteger(R.integer.numberofpoints); i++) {
+    public ArrayList<LatLng> getExample() {
+        ArrayList<LatLng> list = new ArrayList<>();
+        for (double i = 0; i < getResources().getInteger(R.integer.numberofpoints); i++) {
             String path = "R.array.p" + i;
-            int[] temp = getResources().getIntArray(Integer.parseInt(path));
-            list.add(new Point(temp[0], temp[1]));
+            String[] temp = getResources().getStringArray(Integer.parseInt(path));
+            Double[] t= new Double[2];
+            t[0] = Double.parseDouble(temp[0]);
+            t[1] = Double.parseDouble(temp[1]);
+            list.add(new LatLng(t[0], t[1]));
         }
 
         return list;
